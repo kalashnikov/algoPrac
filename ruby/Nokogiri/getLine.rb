ed0f21a404c1e6cb8c4c791d9bc3f291ce2a1cf0
KalaKuo <kalaexj@gmail.com>
2014-05-18 01:10:24 +0800

    update and clean code

--- ruby/Nokogiri/getLine.rb
+++ ruby/Nokogiri/getLine.rb
@@ -4,66 +4,58 @@ require 'open-uri'
 require 'openssl'
 
 #
-# This ruby script get Thumbnail from Top10 pages  
+# This ruby script generate Sticker thumbnail from LINE official site
 #
+
+$OPNAME = ["official","creator"]
+$OPLIST = ["https://store.line.me/stickershop/list?page=", 
+           "https://store.line.me/stickershop/showcase/top_creators?page="]
 
 cookie = open("https://store.line.me/stickershop/list?page=1&listType=top", :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).meta['set-cookie']#.split('; ',2)[0]
                                           
 newck  = "store_locale=zh_TW; store_lang=zh-hant; " + cookie
 
-File.open('demo.html','w') do |output|
-    puts "<!DOCTYPE html>"
-    puts "<html>"
-    puts "<title> Grid Demo </title>"
-    puts "<head>"
-    puts '<link rel="stylesheet" type="text/css" href="css/forGrid.css">'
-    puts "</head>"
-    
-    puts '<div id="wrapper">'
-    puts '<div id="columns">'
+$OPLIST.each do |opl|
+    str = $OPNAME[$OPLIST.index(opl)]
+    File.open("#{str}.html",'w') do |output|
 
-    (1..10).each do |n|
+        (1..10).each do |n|
 
-        link = "https://store.line.me/stickershop/list?page=#{n}&listType=top"
-        #puts "### #{link} ###"
-        page = Nokogiri::HTML(open(link, "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                                         "Accept-Language" => "zh-TW,zh;q=0.8,en;q=0.6,en-US;q=0.4",
-                                         "Cookie" => newck,
-                                         "Connection" => "keep-alive",
-                                         "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36"
-                                         ))
-        page.encoding = 'utf-8'
+            link = "#{opl}#{n}&listType=top"
+            page = Nokogiri::HTML(open(link, "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                                             "Accept-Language" => "zh-TW,zh;q=0.8,en;q=0.6,en-US;q=0.4",
+                                             "Cookie" => newck,
+                                             "Connection" => "keep-alive",
+                                             "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36"
+                                             ))
+            page.encoding = 'utf-8'
 
-        
-        page.css('li.mdMN02Li').each do |o|
-            puts '<div class="pin">'
             
-            imglink = "https://store.line.me" + o.css('a')[0]['href']     # Get detail webpage link
-            imgsrc  = o.css('div.mdMN02Img img')[0]['src']                # Get Image Src Link
-            imgtext = o.css('div.mdMN02Desc')[0].text                     # Get Text
+            page.css('li.mdMN02Li').each do |o|
+                output.puts '<div class="pin">'
+                
+                imglink = "https://store.line.me" + o.css('a')[0]['href']     # Get detail webpage link
+                imgsrc  = o.css('div.mdMN02Img img')[0]['src']                # Get Image Src Link
+                imgtext = o.css('div.mdMN02Desc')[0].text                     # Get Text
 
-            dpage = Nokogiri::HTML(open(imglink, "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                                       "Accept-Language" => "zh-TW,zh;q=0.8,en;q=0.6,en-US;q=0.4",
-                                       "Cookie" => newck,
-                                       "Connection" => "keep-alive",
-                                       "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36"
-                                      ))
-            dpage.encoding = 'utf-8'
+                dpage = Nokogiri::HTML(open(imglink, "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                                           "Accept-Language" => "zh-TW,zh;q=0.8,en;q=0.6,en-US;q=0.4",
+                                           "Cookie" => newck,
+                                           "Connection" => "keep-alive",
+                                           "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36"
+                                          ))
+                dpage.encoding = 'utf-8'
 
-            dtext = dpage.css('p.mdMN07Desc')[0].text
+                dtext = dpage.css('p.mdMN07Desc')[0].text
 
-            puts "\t<a href=\"#{imglink}\">" 
-            puts "\t\t<img src=\"#{imgsrc}\" />"
-            puts "\t</a>" 
-            puts "\t<p> <h3>#{imgtext}</h3><br> #{dtext} </p>" 
+                output.puts "\t<a href=\"#{imglink}\">" 
+                output.puts "\t\t<img src=\"#{imgsrc}\" />"
+                output.puts "\t</a>" 
+                output.puts "\t<p> <h3>#{imgtext}</h3><br> #{dtext} </p>" 
 
-            puts '</div>'
+                output.puts '</div>'
+            end
+            output.puts
         end
-        puts
     end
-    
-    puts "</div>" 
-    puts "</div>" 
-    puts "</html>"
 end
-
