72fa81dc6fd1eeabf5f06b94428e39171414c721
kalashnikov <kalaexj@gmail.com>
2016-12-25 23:49:02 +0800

    1.1.7

--- racket/1.rkt
+++ racket/1.rkt
@@ -2,10 +2,10 @@
 
 ; ----------------------------------------- ;
 ; Play-ground
-(define a 416)
-(println "hello world")
-(println (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5) ) ) ) ) 
-            (* 3 (- 6 2) (- 2 7) ) ) )
+; (define a 416)
+; (println "hello world")
+; (println (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5) ) ) ) ) 
+;             (* 3 (- 6 2) (- 2 7) ) ) )
 
 (define (sq a)
   (* a a)
@@ -24,15 +24,14 @@
         )
   )
 
-(println (e13 3 4 5) )
+; (println (e13 3 4 5) )
 
 ; ----------------------------------------- ;
-; Chapter 1.1.7
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+; Chapter 1.1.7 with improvement
+(define (sqrt-iter guess last x)
+  (if (good-enough? guess last)
     guess 
-    (sqrt-iter (improve guess x)
-               x)))
+    (sqrt-iter (improve guess x) guess x)))
 
 (define (improve guess x)
   (average guess (/ x guess)))
@@ -40,11 +39,34 @@
 (define (average x y)
   (/ (+ x y) 2)) 
 
-(define (good-enough? guess x)
-  (< (abs (- (sq guess) x) ) 0.001) )
+(define (good-enough? guess last)
+  (< (abs (- guess last) ) (* guess 0.001)) )
 
 (define (sqr x)
-  (sqrt-iter 1.0 x))
+  (sqrt-iter 1.0 x x))
 
-(println (sqr 9))
+
+(printf "Squre-root of 10000000: ~a\n" (sqr 10000000))
+(printf "Squre-root of   0.0003: ~a\n" (sqr 0.0003))
+
+; ----------------------------------------- ;
+; Ex-1.8 
+(define (cube-iter guess last x)
+  (if (good-enough? guess last)
+    guess 
+    (cube-iter (cube-improve guess x) guess x)))
+
+(define (cube-improve guess x)
+  (/ (+ (/ x (sq guess)) (* 2 guess)) 3))
+
+(define (cube-root x)
+  (cube-iter 1.0 x x))
+
+(printf " Cube-root of 10000000: ~a\n" (cube-root 10000000))
+(printf " Cube-root of   0.0003: ~a\n" (cube-root 0.0003))
+; ----------------------------------------- ;
+
+
+
+
 ; ----------------------------------------- ;
