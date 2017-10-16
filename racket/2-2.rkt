baa8fa6035dac3287e45a61c5e07c15c7eb3bb60
kalashnikov <kalaexj@gmail.com>
2017-10-16 23:54:11 +0800

    Practice

---
+++ racket/2-2.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define square (list 1 4 9 16 25))
+(define odds (list 1 3 5 7))
+
+(define (list-ref items n)
+  (if (= n 0)
+    (car items)
+    (list-ref (cdr items) (- n 1))))
+
+(define (length items)
+  (if (null? items)
+    0
+    (+ 1 (length (cdr items)))))
+
+
+(define (append list1 list2)
+  (if (null? list1)
+    list2
+    (cons (car list1) (append (cdr list1) list2))))
